09de948dfbe1baa651192e4d481a0a26f85cc809
Ionuț Arțăriși <ionut@artarisi.eu>
2012-06-01 13:50:14 +0200

    use a TEST_URL constant in the tests for DRY

--- shortcats/tests.py
+++ shortcats/tests.py
@@ -5,30 +5,32 @@ import redis
 from shortcats import shorten
 from shortcats.configs import rdb
 
+TEST_URL = 'http://doesn.exist'
 class ShortenTest(unittest.TestCase):
 
     def tearDown(self):
         rdb.flushdb()
 
+    # shorten tests
     def test_shorten_already_exists(self):
-        rdb.set('urls|http://existing.test', 'a1f')
-        self.assertEqual(shorten('http://existing.test'), 'a1f')
+        rdb.set('urls|'+TEST_URL, 'a1f')
+        self.assertEqual(shorten(TEST_URL), 'a1f')
 
     def test_shorten_doesnt_exist_returns_valid(self):
-        self.assertIsNone(rdb.get('urls|http://doesnt.exist'))
-        self.assertEqual(shorten('http://doesnt.exist'), '1')
+        self.assertIsNone(rdb.get('urls|'+TEST_URL))
+        self.assertEqual(shorten(TEST_URL), '1')
 
     def test_shorten_doesnt_exist_returns_next(self):
         rdb.set('url_counter', 51)
-        self.assertEqual(shorten('http://doesnt.exist'), '1g')
+        self.assertEqual(shorten(TEST_URL), '1g')
 
     def test_shorten_doesnt_exist_creates_new(self):
         with self.assertRaises(KeyError):
-            rdb['urls|http://doesn.exist']
-        shorten('http://doesn.exist')
-        self.assertEqual(rdb.get('urls|http://doesn.exist'), '1')
+            rdb['urls|'+TEST_URL]
+        short = shorten(TEST_URL)
+        self.assertEqual(rdb.get('urls|'+TEST_URL), '1')
 
     def test_shorten_doesnt_exist_creates_new_next(self):
         rdb.set('url_counter', 51)
-        shorten('http://doesn.exist')
-        self.assertEqual(rdb.get('urls|http://doesn.exist'), '1g')
+        shorten(TEST_URL)
+        self.assertEqual(rdb.get('urls|'+TEST_URL), '1g')
